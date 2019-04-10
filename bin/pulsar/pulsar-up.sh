035f15603ae4d05ebf64e9984eb5ab2aea9dc988
Markus Paasovaara <markus.paasovaara@futurice.com>
2019-04-10 15:02:47 +0300

    Pulsar to 2.2.1

--- bin/pulsar/pulsar-up.sh
+++ bin/pulsar/pulsar-up.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker run -d -p 127.0.0.1:6650:6650 -p 127.0.0.1:8089:8080 -v $PWD/data:/pulsar/data --rm --name pulsar  apachepulsar/pulsar:2.1.0-incubating bin/pulsar standalone --advertised-address 127.0.0.1
+docker run -d -p 127.0.0.1:6650:6650 -p 127.0.0.1:8089:8080 -v $PWD/data:/pulsar/data --rm --name pulsar  apachepulsar/pulsar:2.2.1 bin/pulsar standalone --advertised-address 127.0.0.1
 
